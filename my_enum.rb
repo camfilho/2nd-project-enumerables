4099799b04be6f5a78427fcc13a24acf8885f53f
camfilho <cfmedeiros@ufn.edu.br>
2019-11-02 00:34:26 -0300

    finish my_each

--- my_enum.rb
+++ my_enum.rb
@@ -1,3 +1,15 @@
-def my_each
+# frozen_string_literal: true
 
+module Enumerable
+  def my_each
+    return to_enum unless block_given?
+
+    object = dup
+    i = 0
+    while i < length
+      yield(object.shift)
+      i += 1
+    end
+    self
+  end
 end
