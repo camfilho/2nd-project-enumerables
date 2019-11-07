e3a5677d2f3c6682db64455d3af8ddd6d9d88c42
camfilho <cfmedeiros@ufn.edu.br>
2019-11-07 17:18:37 -0300

    fixe some Rubocop errors

--- my_enum.rb
+++ my_enum.rb
@@ -12,4 +12,32 @@ module Enumerable
     end
     self
   end
+
+  def my_map
+    return to_enum unless block_given?
+
+    copy_arr = dup
+    new_object = self.class.new
+    0.upto(length - 1) do
+      result = yield(copy_arr.shift)
+      if result.is_a? Array
+        new_object[result[0]] = result[1]
+      else
+        new_object.push result
+      end
+    end
+    new_object
+  end
+
+  def my_each_with_index
+    return to_enum unless block_given?
+
+    object = dup
+    i = 0
+    while i < length
+      yield(object.shift, i)
+      i += 1
+    end
+    self
+  end
 end
